aa3e345c26986dc7bafa7184b6c775ea24c06234
serhat-bca <sersen@bergen.org>
2024-09-06 15:56:34 -0400

    intro

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,8 +1,29 @@
-const App = () => {
+const ReactLogo = (props) => {
   return (
-    <div>
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
+      alt=""
+      width={props.size}
+    />
+  );
+};
+
+const App = () => {
+  const releaseYear = 2013;
+  const developerCompany = "Facebook, Inc.";
+  console.log("Release year: ", releaseYear);
+  console.log(`Developer company: ${developerCompany}`);
+  console.log("Hello from App component");
+  return (
+    <>
       <h1>Hello React!!!</h1>
-    </div>
+      <ReactLogo size="100" />
+      <ReactLogo size="75" />
+      <p>
+        React was developed by {developerCompany} and was first released{" "}
+        {releaseYear}, {new Date().getFullYear() - releaseYear} years ago.
+      </p>
+    </>
   );
 };
 
