20a7065c0a0da8f7b1d07a8fc3243b99659fae88
serhat-bca <sersen@bergen.org>
2024-09-10 15:31:38 -0400

    object props

--- contacts/src/App.jsx
+++ contacts/src/App.jsx
@@ -1,25 +1,29 @@
+/* eslint-disable react/prop-types */
 const Contact = (props) => {
   return (
     <div>
-      Name: {props.name}
+      Name: {props.contact.name}
       <br />
-      DOB: {props.dob}
+      DOB: {props.contact.dob}
       <br />
-      State: {props.state}
+      State: {props.contact.state}
       <hr />
     </div>
   );
 };
 
 const App = () => {
+  const john = { name: "John Doe", dob: "Jan 2001", state: "NJ" };
+  const jane = { name: "Jane Doe", dob: "May 1999", state: "NJ" };
+  const kane = { name: "Kane Doe", dob: "July 2005", state: "NJ" };
+  const wayne = { name: "Wayne Doe", dob: "Sep 2006", state: "NJ" };
   return (
     <>
-      {" "}
       <h1>Contacts</h1>
-      <Contact name="John Doe" state="NJ" dob="Jan 2001" />
-      <Contact name="Jane Doe" state="NJ" dob="May 1999" />
-      <Contact name="Kane Doe" state="NJ" dob="July 2005" />
-      <Contact name="Wayne Doe" state="NJ" dob="Sep 2006" />
+      <Contact contact={john} />
+      <Contact contact={jane} />
+      <Contact contact={kane} />
+      <Contact contact={wayne} />
     </>
   );
 };
