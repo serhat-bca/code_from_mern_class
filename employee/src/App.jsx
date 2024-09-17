835579ab1f151068e0419f384ebeb5ba79c539b7
serhat-bca <sersen@bergen.org>
2024-09-17 15:34:38 -0400

    functions added.

--- employee/src/App.jsx
+++ employee/src/App.jsx
@@ -1,8 +1,26 @@
 /* eslint-disable react/prop-types */
 const Employee = (props) => {
+  const weekly = () => {
+    return moneyFormat(props.hrly * 40);
+  };
+
+  const moneyFormat = (amount) => {
+    return `$${amount.toLocaleString("en-US")}`;
+  };
+
+  const annual = () => {
+    return moneyFormat(props.hrly * 40 * 52);
+  };
+
   return (
     <p>
       {props.name} works at {props.department}.
+      <br />
+      Hourly Rate: {props.hrly}
+      <br />
+      Weekly Rate: {weekly()}
+      <br />
+      Annual Rate: {annual()}
     </p>
   );
 };
@@ -11,7 +29,7 @@ const App = () => {
   return (
     <div>
       <h1>Employees</h1>
-      <Employee name="John Doe" department="IT Support" />
+      <Employee name="John Doe" department="IT Support" hrly={27.45} />
     </div>
   );
 };
