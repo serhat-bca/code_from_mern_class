46e6380ce304600dd64ff9a5100675fa598cd6a4
serhat-bca <sersen@bergen.org>
2024-09-20 12:16:59 -0400

    javascript.

--- counter-state/src/App.jsx
+++ counter-state/src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  // javascript array destructuring
+  const arr = [10, 20, 30, 40, 50, 60, 70];
+  const [a, b, c, ...d] = arr;
+  console.log("a: ", a, "b: ", b, "c: ", c, "d: ", d);
+
+  const stateLike = (value) => {
+    const printer = (a) => console.log("Parameter", a);
+    return [value, printer];
+  };
+
+  const [x, y] = stateLike("Hello");
+  console.log("x: ", x);
+  y("xyz");
 
   const increase = () => {
     console.log("counter", counter);
